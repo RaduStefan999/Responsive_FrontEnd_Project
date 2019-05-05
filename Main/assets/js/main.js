fb7187c0108b77ab0b2002937d459c9d5fc571c3
raducu999 <radustefan1302@gmail.com>
2019-05-05 17:48:45 +0300

    menu-section

--- Main/assets/js/main.js
+++ Main/assets/js/main.js
@@ -4,29 +4,44 @@ $(document).ready(function(){
 
     var $content = $("#content");
 
-    var $nav_section_menu = $("#nav-section-menu");
 
     var $menu = $("#nav_menu");
     var $menu_responsive = $("#responsive_menu");
+    var $nav_section_menu = $("#nav-section-menu");
+
     var $search = $("#nav_search");
     var $search_responsive = $("#responsive_search");
+    var $nav_section_search = $("#nav-section-search");
+
     var $contact = $("#nav_contact");
     var $contact_responsive = $("#responsive_contact");
+    var $nav_section_contact = $("#nav-section-contact");
+
     var $referenser = $("#nav_referenser");
     var $referenser_resposnive = $("#responsive_referenser");
+    var $nav_section_referenser = $("#nav-section-referenser");
+
     var $sustainability = $("#nav_sustainability");
     var $sustainability_responsive = $("#responsive_sustainability");
+    var $nav_section_sustainability = $("#nav-section-sustainability");
+
 
     $menu.click(function(){ nav_general($menu, $nav_section_menu); });
     $menu_responsive.click(function(){ nav_general($menu, $nav_section_menu); });
-    $search.click(function(){ nav_general($search, $nav_section_menu); });
-    $search_responsive.click(function(){ nav_general($search, $nav_section_menu); });
-    $contact.click(function(){ nav_general($contact, $nav_section_menu); });
-    $contact_responsive.click(function(){ nav_general($contact, $nav_section_menu); });
-    $referenser.click(function(){ nav_general($referenser, $nav_section_menu); });
-    $referenser_resposnive.click(function(){ nav_general($referenser, $nav_section_menu); });
-    $sustainability.click(function(){ nav_general($sustainability, $nav_section_menu); });
-    $sustainability_responsive.click(function(){ nav_general($sustainability, $nav_section_menu); });
+
+    $search.click(function(){ nav_general($search, $nav_section_search); });
+    $search_responsive.click(function(){ nav_general($search, $nav_section_search); });
+
+    $contact.click(function(){ nav_general($contact, $nav_section_contact); });
+    $contact_responsive.click(function(){ nav_general($contact, $nav_section_contact); });
+
+    $referenser.click(function(){ nav_general($referenser, $nav_section_referenser); });
+    $referenser_resposnive.click(function(){ nav_general($referenser, $nav_section_referenser); });
+
+    $sustainability.click(function(){ nav_general($sustainability, $nav_section_sustainability); });
+    $sustainability_responsive.click(function(){ nav_general($sustainability, $nav_section_sustainability); });
+
+
 
     function nav_general ($element, $target) {
       $element.toggleClass("active-navi");
@@ -37,19 +52,19 @@ $(document).ready(function(){
       }
       if ($element != $search) {
         $search.removeClass("active-navi");
-        $nav_section_menu.addClass("hidden");
+        $nav_section_search.addClass("hidden");
       }
       if ($element != $contact) {
         $contact.removeClass("active-navi");
-        $nav_section_menu.addClass("hidden");
+        $nav_section_contact.addClass("hidden");
       }
       if ($element != $referenser) {
         $referenser.removeClass("active-navi");
-        $nav_section_menu.addClass("hidden");
+        $nav_section_referenser.addClass("hidden");
       }
       if ($element != $sustainability) {
         $sustainability.removeClass("active-navi");
-        $nav_section_menu.addClass("hidden");
+        $nav_section_sustainability.addClass("hidden");
       }
 
       if ($element.hasClass("active-navi")) {
